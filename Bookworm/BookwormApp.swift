058d5738315ddd8acbba7f8d7d32174d638d797a
Maria Sandu <Maria.Sandu@emids.com>
2023-09-19 12:58:44 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Maria Sandu on 19.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
